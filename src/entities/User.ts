29b6d1332d745c898797f73a43e780835fdcfa11
plusbeauxjours <plusbeauxjours@gmail.com>
2018-11-06 08:12:11 +0100

    #1.15 Hashing and Encrypting User Passwords

--- src/entities/User.ts
+++ src/entities/User.ts
@@ -1,12 +1,17 @@
+import bcrypt from "bcrypt";
 import { IsEmail } from "class-validator";
 import { 
-    Entity, 
     BaseEntity, 
-    PrimaryGeneratedColumn, 
+    BeforeInsert,
+    BeforeUpdate,
     Column, 
+    CreateDateColumn, 
+    Entity, 
+    PrimaryGeneratedColumn, 
     UpdateDateColumn, 
-    CreateDateColumn 
 } from "typeorm";
+
+const BCRYPT_ROUNDS = 10;
 
 @Entity()
 class User extends BaseEntity {
@@ -58,12 +63,25 @@ class User extends BaseEntity {
     @Column({ type: "double precision", default: 0})
     lastOrientation: number;
     
+    @CreateDateColumn() createdAt: string;
+    @UpdateDateColumn() updatedAt: string;
+    
     get fullName(): string {
         return `${this.firstName} ${this.lastName}`
     }
 
-    @CreateDateColumn() createdAt: string;
-    @UpdateDateColumn() updatedAt: string;
+    @BeforeInsert()
+    @BeforeUpdate()
+    async savePassword(): Promise<void> {
+        if(this.password) {
+            const hashedPassword = await this.hashPassword(this.password);
+            this.password = hashedPassword;
+        }
+    }
+
+    private hashPassword(password: string): Promise<string> {
+        return bcrypt.hash(password, BCRYPT_ROUNDS);
+    }
 }
 
 export default User;
