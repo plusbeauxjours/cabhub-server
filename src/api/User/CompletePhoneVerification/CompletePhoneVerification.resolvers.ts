223ff59663ff6ff09c98268de7bf4b0492d64ab7
plusbeauxjours <plusbeauxjours@gmail.com>
2018-11-10 06:08:32 +0100

    #1.39 CompletePhoneVerification part Two

--- src/api/User/CompletePhoneVerification/CompletePhoneVerification.resolvers.ts
+++ src/api/User/CompletePhoneVerification/CompletePhoneVerification.resolvers.ts
@@ -1,15 +1,57 @@
 import { Resolvers } from '../../../types/resolvers';
-import { CompletePhoneVerificationMutationArgs, CompletePhoneVerificationResponse } from '../../../types/graph';
+import { 
+    CompletePhoneVerificationMutationArgs, 
+    CompletePhoneVerificationResponse 
+} from '../../../types/graph';
+import User from '../../../entities/User';
+import Verification from '../../../entities/Verification';
 
 const resolvers: Resolvers = {
     Mutation: {
         CompletePhoneVerification: async (
             _,
             args: CompletePhoneVerificationMutationArgs
-        ): Promise<CompletePhoneVerificationResponse> => {
-            const { phoneNumber } = args;
+        ) : Promise<CompletePhoneVerificationResponse> => {
+            const { phoneNumber, key } = args;
             try {
-
+                const verification = await Verification.findOne({
+                    payload: phoneNumber,
+                    key
+                })
+                if (!verification) {
+                    return {
+                        ok: false,
+                        error: "Verification key not valid",
+                        token: null
+                    }
+                } else {
+                    verification.verified = true;
+                    verification.save()
+                }
+            } catch (error) {
+                return { 
+                    ok: false,
+                    error: error.message,
+                    token: null
+                };
+            }
+            try {
+                const user = await User.findOne({ phoneNumber });
+                if(user) {
+                    user.verifiedPhonenNumber = true;
+                    user.save()
+                    return {
+                        ok: true,
+                        error: null,
+                        token: "Coming soon"
+                    }
+                } else {
+                    return {
+                        ok: true,
+                        error: null,
+                        token: null
+                    }
+                }
             } catch (error) {
                 return {
                     ok: false,
