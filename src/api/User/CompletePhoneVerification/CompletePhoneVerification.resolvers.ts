9cb02704441bc1e02c7fccc3f3ae76724d7eea43
plusbeauxjours <plusbeauxjours@gmail.com>
2018-11-10 05:08:51 +0100

    #.1.38 CompletePhoneVerification part One

---
+++ src/api/User/CompletePhoneVerification/CompletePhoneVerification.resolvers.ts
@@ -0,0 +1,24 @@
+import { Resolvers } from '../../../types/resolvers';
+import { CompletePhoneVerificationMutationArgs, CompletePhoneVerificationResponse } from '../../../types/graph';
+
+const resolvers: Resolvers = {
+    Mutation: {
+        CompletePhoneVerification: async (
+            _,
+            args: CompletePhoneVerificationMutationArgs
+        ): Promise<CompletePhoneVerificationResponse> => {
+            const { phoneNumber } = args;
+            try {
+
+            } catch (error) {
+                return {
+                    ok: false,
+                    error: error.message,
+                    token: null
+                }
+            }
+        }
+    }
+}
+
+export default resolvers;
