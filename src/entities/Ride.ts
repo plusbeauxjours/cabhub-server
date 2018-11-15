9d818b3d3c82fc6b70d26d4224c1225be92e8b80
plusbeauxjours <plusbeauxjours@gmail.com>
2018-11-15 20:56:12 +0100

    #1.78 GetRide Resolver

--- src/entities/Ride.ts
+++ src/entities/Ride.ts
@@ -16,7 +16,7 @@ class Ride extends BaseEntity {
 
     @Column({
         type: "text", enum: ["ACCEPTED", "FINISHED", "CANCELED", "REQUESTING", "ONROUTE"],
-        default : "ACCEPTED"
+        default : "REQUESTING"
     })
     status: rideStatus;
 
@@ -47,8 +47,14 @@ class Ride extends BaseEntity {
     @Column({ type: "text"})
     duration: string;
 
+    @Column({ nullable: true })
+    driverId: number;
+
     @ManyToOne( type => User, user => user.rideAsDriver, { nullable: true } )
     driver: User;
+
+    @Column({  nullable: true})
+    passengerId: number;
 
     @ManyToOne( type => User, user => user.rideAsPassenger )
     passenger: User;
